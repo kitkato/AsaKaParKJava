9b28de919beb85227aedfa73f28644b892b71454
Zane <it211.sonsona@gmail.com>
2020-11-13 21:49:50 +0800

    Updated Fragments

--- app/src/main/java/com/moke/asakapark/LoginPage.java
+++ app/src/main/java/com/moke/asakapark/LoginPage.java
@@ -2,7 +2,10 @@ package com.moke.asakapark;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        Button loginbtn = findViewById(R.id.login);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginPage.this, MainActivity.class));
+            }
+        });
+
     }
 }
