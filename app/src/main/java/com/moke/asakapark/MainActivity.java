16aac3cdef27ec2205991652a287e6d682cd82cf
Morley Cellan <morleycellan@gmail.com>
2020-11-13 21:27:19 +0800

    btm navigation

--- app/src/main/java/com/moke/asakapark/MainActivity.java
+++ app/src/main/java/com/moke/asakapark/MainActivity.java
@@ -1,8 +1,14 @@
 package com.moke.asakapark;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.homeFragment, R.id.parkFragment, R.id.carFragment, R.id.personFragment).build();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
